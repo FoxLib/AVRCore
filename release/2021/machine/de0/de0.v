adc0a4da36383c71db0d5b6452b6b99174d6e22e
FoxLib <hplus.foxtail@gmail.com>
2021-03-24 17:20:06 +0300

    Добавление модуля флеш-памяти

--- release/2021/machine/de0/de0.v
+++ release/2021/machine/de0/de0.v
@@ -2,13 +2,13 @@ module de0(
 
     // Reset
     input              RESET_N,
-    
+
     // Clocks
     input              CLOCK_50,
     input              CLOCK2_50,
     input              CLOCK3_50,
     inout              CLOCK4_50,
-    
+
     // DRAM
     output             DRAM_CKE,
     output             DRAM_CLK,
@@ -21,11 +21,11 @@ module de0(
     output             DRAM_CS_N,
     output             DRAM_LDQM,
     output             DRAM_UDQM,
-    
+
     // GPIO
     inout       [35:0] GPIO_0,
     inout       [35:0] GPIO_1,
-    
+
     // 7-Segment LED
     output      [6:0]  HEX0,
     output      [6:0]  HEX1,
@@ -33,27 +33,27 @@ module de0(
     output      [6:0]  HEX3,
     output      [6:0]  HEX4,
     output      [6:0]  HEX5,
-    
+
     // Keys
     input       [3:0]  KEY,
-    
+
     // LED
     output      [9:0]  LEDR,
-    
+
     // PS/2
     inout              PS2_CLK,
     inout              PS2_DAT,
     inout              PS2_CLK2,
     inout              PS2_DAT2,
-    
+
     // SD-Card
     output             SD_CLK,
     inout              SD_CMD,
     inout       [3:0]  SD_DATA,
-    
+
     // Switch
     input       [9:0]  SW,
-    
+
     // VGA
     output      [3:0]  VGA_R,
     output      [3:0]  VGA_G,
@@ -84,9 +84,58 @@ de0pll unit_pll
 (
     .clkin     (CLOCK_50),
     .m25       (clock_25),
-    .m100      (clock_100),
+    .m100      (clock),
     .locked    (locked)
 );
+
+// ---------------------------------------------------------------------
+// Объявление процессора
+// ---------------------------------------------------------------------
+
+wire [15:0] pc;         // Программный счетчик
+reg  [15:0] ir;         // Регистр инструкции (текущий опкод)
+wire [15:0] address;    // Адрес в памяти SRAM
+reg  [ 7:0] data_i;     // Прочтенные данные из SRAM
+wire [ 7:0] data_o;     // Данные для записи в SRAM
+wire        wren;       // Разрешение на запись в память
+
+// @todo Роутинг памяти >> data_i
+
+avrcpu ModuleCPU
+(
+    // Тактовый генератор
+    .clock      (clock_25 & locked),
+
+    // Программная память
+    .pc         (pc),           // Программный счетчик
+    .ir         (ir),           // Инструкция из памяти
+
+    // Оперативная память
+    .address    (address),      // Указатель на память RAM (sram)
+    .data_i     (data_i),       // memory[ address ]
+    .data_o     (data_o),       // Запись в память по address
+    .wren       (wren)          // Разрешение записи в память
+);
+
+// ---------------------------------------------------------------------
+// Модули внутрисхемной памяти
+// ---------------------------------------------------------------------
+
+// 128k Память программ
+memflash UnitMemFlash
+(
+    .clock     (clock),
+    .address_a (pc),
+    .q_a       (ir),
+);
+
+// 8k   Видеопамять текстового режима
+// 128k Видеопамять графического режима
+// 32k  Дополнительная память
+
+// ---------------------------------------------------------------------
+// Видеоадаптер
+// ---------------------------------------------------------------------
 
 vga unit_vga
 (
@@ -99,3 +148,5 @@ vga unit_vga
 );
 
 endmodule
+
+`include "../avrcpu.v"
